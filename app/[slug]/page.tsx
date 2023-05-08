92adfb50fbe5b8ded22f77b9c6d444da7ead9002
junayd-bhoyroo <hello@junaydbhoyroo.com>
2023-05-09 00:32:14 +0100

    bug

---
+++ app/[slug]/page.tsx
@@ -0,0 +1,5 @@
+export const dynamicParams = false;
+
+export default function Page() {
+    return null;
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
 /** Add your relevant code here for the issue to reproduce */
+export const dynamicParams = false;
+
 export default function Home() {
-  return null
+    return null;
 }
